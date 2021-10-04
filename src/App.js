a658bcb6017ffa9b11e228c43fbbedbf1910c076
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2021-10-05 00:24:36 +0900

    [#1] [MainPage] 홈 시작 화면 - App.js에 BackTop 추가 - 이미지는 아직 디자인x, Carousel 이용

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import LoginPage from "./components/views/Login/Login";
 import ResearchArea from "./components/views/ResearchArea/ResearchArea";
 import Members from "./components/views/Members/Members";
 import Footer from "./components/views/Footer/Footer";
+import { BackTop } from "antd";
 
 function App() {
   return (
@@ -13,8 +14,8 @@ function App() {
       <NavBar />
       <div
         style={{
-          padding: "100px 200px 30px 200px",
-          minHeight: "calc(200vh - 80px)",
+          padding: "100px 0px 30px 0px", // "100px 200px 30px 200px"
+          minHeight: "", // "calc(200vh - 80px)"
         }}
       >
         <Switch>
@@ -24,6 +25,7 @@ function App() {
           <Route exact path="/members" component={Members} />
         </Switch>
       </div>
+      <BackTop />
       <Footer />
     </BrowserRouter>
   );
--- src/components/views/MainPage/MainPage.jsx
+++ src/components/views/MainPage/MainPage.jsx
@@ -1,12 +1,35 @@
 import React from "react";
-import { Layout, Menu } from "antd";
+import { Carousel, BackTop } from "antd";
 import "antd/dist/antd.css";
 import styles from "./MainPage.module.css";
 
 function MainPage() {
+  const contentStyle = {
+    height: "800px",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    background: "#364d79",
+  };
+
   return (
     <>
-      <div className={styles.container}>mainpage</div>
+      <div className={styles.carousel}>
+        <Carousel autoplay>
+          <div>
+            <h3 style={contentStyle}>1</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>2</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>3</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>4</h3>
+          </div>
+        </Carousel>
+      </div>
     </>
   );
 }
