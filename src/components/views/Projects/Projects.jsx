f32a4ca7a9b04456c993b40cd4d83510ea7d4dc2
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2021-10-30 19:56:26 +0900

    [#6] Project-Budget 테이블 추가

--- src/components/views/Projects/Projects.jsx
+++ src/components/views/Projects/Projects.jsx
@@ -2,12 +2,17 @@ import React, { useEffect } from "react";
 import { Row, Col } from "antd";
 import styles from "@projects/Projects.module.css";
 import Typography from "@mui/material/Typography";
-import OngoingProject from "@projects/sections/OngoingProject";
-import { ongoingList } from "@projects/sections/OngoingData";
+import ProjectBudget from "@projects/sections/projectBudget/ProjectBudget";
+import OngoingProject from "@projects/sections/ongoing/OngoingProject";
+import { ongoingList } from "@projects/sections/ongoing/OngoingData";
 
 function Projects() {
   return (
     <div className={styles.container}>
+      <Typography variant="h5">
+        <b>프로젝트 소개</b>
+      </Typography>
+      <ProjectBudget />
       <Typography variant="h6">
         <b>진행 중인 연구 프로젝트</b>
       </Typography>
